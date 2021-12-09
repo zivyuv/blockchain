1e12491e5e90121c34628e83c55e379451c4d535
sahar <sahar576cohen@gmail.com>
2021-12-09 23:50:16 +0200

    some more updates

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,28 +1,26 @@
 import CardStyle from './Card.module.css'
 import React from 'react';
 
-const Card = ({header, content, price, submitHandler}) => {
-
-    // const strPrice = "Buy" + price
+const Card = ({header, content, price, submitHandler}) => { // const strPrice = "Buy" + price
     const strPrice = "Buy\tfor\t" + 19 + "$"
     return (
-        <div>
-            <div className={CardStyle.card}>
-                <h1>What I'm offering</h1>
-                <p>About me....About me....About me....About me....About me....About me....About me....About me....</p>
-                <p>Some text about the the offer..Some text about the the offer..Some text about the the offer..Some text about the the offer..Some text about the the offer..</p>
-                <p>Some text about the the offer..</p>
-                <p>Some text about the the offer..</p>
-                <p>Some text about the the offer..</p>
-                <p>Some text about the the offer..</p>
-
-                    <div className="container"> 
-                        <h5 className="badge badge-success" >my rate</h5>
-                        <h5 className="badge badge-success" >times bought</h5>
-                        <a className="badge badge-success" href="https://www.google.com/">link to social / prev work</a>
-
-                        <span><button>{strPrice}</button></span>
-                    </div>
+        <div className="card border-success mb-3"
+            style={
+                {margin: "2rem"}
+        }>
+            <div className="card-header bg-transparent border-success">Header</div>
+            <div className="card-body text-success">
+                <h5 className="card-title">Success card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <div className="card-footer bg-transparent ">
+                <button className="btn btn-dark btn-lg btn-block">sadfasd</button>
+            </div>
+            <div className="card-footer bg-transparent ">
+                <div className="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" className="btn btn-primary">Rate Seller</button>
+                    <button type="button" className="btn btn-info">To social media</button>
+                </div>
             </div>
         </div>
     );
